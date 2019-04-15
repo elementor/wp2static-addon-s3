c373e70e8eff9023e36c76384ef8f2dd0452e700
Leon <leon@wp2static.com>
2019-04-15 20:11:33 +0700

    update HTML and JS

--- admin/js/wp2static-addon-s3-admin.js
+++ admin/js/wp2static-addon-s3-admin.js
@@ -5,19 +5,20 @@
     deploy_options['s3'] = {
       exportSteps: [
           's3_prepare_export',
-          's3_upload_files',
+          's3_transfer_files',
+          'cloudfront_invalidate_all_items',
           'finalize_deployment'
       ],
       required_fields: {
-        azStorageAccountName: 'Please specify your Storage Account Name in order to deploy to S3.',
-        azContainerName: 'Please specify your Container Name in order to deploy to S3.',
-        azAccessKey: 'Please specify your Access Key for this Storage/Container.'
+        s3Key: 'Please input an S3 Key in order to authenticate when using the S3 deployment method.',
+        s3Secret: 'Please input an S3 Secret in order to authenticate when using the S3 deployment method.',
+        s3Bucket: 'Please input the name of the S3 bucket you are trying to deploy to.',
       }
     };
 
-    status_descriptions['s3_prepare_export'] = 'Preparing to deploy to Microsoft S3 Storage';
-    status_descriptions['s3_upload_files'] = 'Uploading files to Microsoft S3 Storage';
-
+    status_descriptions['s3_prepare_export'] = 'Preparing files for S3 deployment';
+    status_descriptions['s3_transfer_files'] = 'Deploying files to S3';
+    status_descriptions['cloudfront_invalidate_all_items'] = 'Invalidating CloudFront cache';
   }); // end DOM ready
 
 })( jQuery );
