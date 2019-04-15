da7ba71432839228d326544fb106b77ab93f9a8b
Leon <leon@wp2static.com>
2019-04-15 19:47:50 +0700

    more renaming

--- wp2static-addon-s3.php
+++ wp2static-addon-s3.php
@@ -9,7 +9,7 @@
  * Author URI:        https://leonstafford.github.io
  * License:           Unlicense
  * License URI:       http://unlicense.org
- * Text Domain:       wp2static-addon-azure
+ * Text Domain:       wp2static-addon-s3
  * Domain Path:       /languages
  */
 
@@ -26,21 +26,21 @@ $wp2static_core_dir =
 
 $add_on_dir = dirname( __FILE__ );
 // NOTE: bypass instantiating plugin for specific AJAX requests
-if ( $ajax_action === 'test_azure' ) {
+if ( $ajax_action === 'test_s3' ) {
     require_once $wp2static_core_dir .
         '/plugin/WP2Static/SitePublisher.php';
     require_once $add_on_dir . '/S3Deployer.php';
 
     wp_die();
     return null;
-} else if ( $ajax_action === 'azure_prepare_export' ) {
+} else if ( $ajax_action === 's3_prepare_export' ) {
     require_once $wp2static_core_dir .
         '/plugin/WP2Static/SitePublisher.php';
     require_once $add_on_dir . '/S3Deployer.php';
 
     wp_die();
     return null;
-} else if ( $ajax_action === 'azure_upload_files' ) {
+} else if ( $ajax_action === 's3_upload_files' ) {
     require_once $wp2static_core_dir .
         '/plugin/WP2Static/SitePublisher.php';
     require_once $add_on_dir . '/S3Deployer.php';
@@ -51,27 +51,27 @@ if ( $ajax_action === 'test_azure' ) {
 
 define( 'PLUGIN_NAME_VERSION', '0.1' );
 
-function activate_wp2static_addon_azure() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-wp2static-addon-azure-activator.php';
+function activate_wp2static_addon_s3() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-wp2static-addon-s3-activator.php';
 	Wp2static_Addon_S3_Activator::activate();
 }
 
-function deactivate_wp2static_addon_azure() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-wp2static-addon-azure-deactivator.php';
+function deactivate_wp2static_addon_s3() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-wp2static-addon-s3-deactivator.php';
 	Wp2static_Addon_S3_Deactivator::deactivate();
 }
 
-register_activation_hook( __FILE__, 'activate_wp2static_addon_azure' );
-register_deactivation_hook( __FILE__, 'deactivate_wp2static_addon_azure' );
+register_activation_hook( __FILE__, 'activate_wp2static_addon_s3' );
+register_deactivation_hook( __FILE__, 'deactivate_wp2static_addon_s3' );
 
-require plugin_dir_path( __FILE__ ) . 'includes/class-wp2static-addon-azure.php';
+require plugin_dir_path( __FILE__ ) . 'includes/class-wp2static-addon-s3.php';
 
-function run_wp2static_addon_azure() {
+function run_wp2static_addon_s3() {
 
 	$plugin = new Wp2static_Addon_S3();
 	$plugin->run();
 
 }
 
-run_wp2static_addon_azure();
+run_wp2static_addon_s3();
 
