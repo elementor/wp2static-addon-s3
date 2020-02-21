96591f0e56161b25287c3fa50ed2fbd8dcfc155b
Leon <me@ljs.dev>
2020-02-22 01:29:10 +1030

    fix ignore

--- vendor/giacocorsiglia/wordpress-stubs/finder.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return \StubsGenerator\Finder::create()
-    ->in('wordpress')
-    ->notPath('wp-admin/includes/noop.php')
-    ->notPath('wp-content')
-    ->notPath('wp-includes/spl-autoload-compat.php')
-    ->sortByName()
-;
