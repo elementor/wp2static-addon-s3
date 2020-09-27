c52b06b63310e2d7eefef2d7c434bdc2b85c3a94
Leon Stafford <me@ljs.dev>
2020-09-28 06:06:39 +0930

    quieten build script

--- tools/build_release.sh
+++ tools/build_release.sh
@@ -20,12 +20,10 @@ mkdir -p $TMP_DIR
 rm -Rf $TMP_DIR/wp2static-addon-s3
 mkdir $TMP_DIR/wp2static-addon-s3
 
-
 # clear dev dependencies
 rm -Rf $EXEC_DIR/vendor/*
 # load prod deps and optimize loader
-composer install --no-dev --optimize-autoloader
-
+composer install --quiet --no-dev --optimize-autoloader
 
 # cp all required sources to build dir
 cp -r $EXEC_DIR/*.php $TMP_DIR/wp2static-addon-s3/
@@ -40,11 +38,6 @@ cd $TMP_DIR
 find . -type d -exec chmod 755 {} \;
 find . -type f -exec chmod 644 {} \;
 
-# strip comments and whitespace from each PHP file
-if [ -z "$2" ]; then
-  find .  ! -name 'wp2static-addon-s3.php' -name \*.php -exec $EXEC_DIR/tools/compress_php_file {} \;
-fi
-
 zip -r -9 ./$1.zip ./wp2static-addon-s3
 
 cd -
@@ -58,4 +51,4 @@ cd $EXEC_DIR
 # clear dev dependencies
 rm -Rf $EXEC_DIR/vendor/*
 # load prod deps
-composer install
+composer install --quiet
